0a0c79a9df8f7b78cd43fef305eeff8f42340d47
WeaponOwl <misha@serverdev.net>
2018-12-20 17:27:54 +0200

    trying implement instructions...

--- js/core.js
+++ js/core.js
@@ -58,15 +58,297 @@ if (window.jQuery === undefined) jQuery = $ = {};
 		}
 	};
 
-	Kvm.Class = function(blob) 
+	Kvm.Instructions = function(codes)
+	{
+		var operations = [];
+
+		for(var i=0;i<codes.length;)
+		{
+			var commandCode = codes[i++];
+			var command = { code: commandCode, name: commandCode.toString(16), param: [] };
+			switch(commandCode)
+			{
+				//aaload
+				//aastore
+				//aconst_null
+				//aload
+
+				case 0x2a: 
+					command.name = 'aload_0';
+					break;
+				case 0x2b: 
+					command.name = 'aload_1';
+					break;
+				case 0x2c: 
+					command.name = 'aload_2';
+					break;
+				case 0x2d: 
+					command.name = 'aload_3';
+					break;
+
+				//anewarray
+				//areturn
+				//arraylength
+				//astore
+				//astore_n
+				//athrow
+				//baload
+				//bastore
+
+				case 0x10: 
+					command.name = 'bipush';
+					command.param.push(codes[i++]);
+					break;
+
+				//caload
+				//castore
+				//checkcast
+				//d2f
+				//d2i
+				//d2l
+				//dadd
+				//daload
+				//dastore
+				//dcmp_op
+				//dconst_d
+				//ddiv
+				//dload
+				//dload_n
+				//dmul
+				//dneg
+				//drem
+				//dreturn
+				//dstore
+				//dstore_n
+				//dsub
+				//dup
+				//dup_x1
+				//dup_x2
+				//dup2
+				//dup2_x1
+				//dup2_x2
+				//f2d
+				//f2i
+				//f2l
+				//fadd
+				//faload
+				//fastore
+				//fcmp_op
+				//fconst_f
+				//fdiv
+				//fload
+				//fload_n
+				//fmul
+				//fneg
+				//frem
+				//freturn
+				//fstore
+				//fstore_n
+				//fsub
+				//getfield
+				case 0xb2: 
+					command.name = 'getstatic';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0xa7: 
+					command.name = 'goto';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				//goto_w
+				//i2b
+				//i2c
+				//i2d
+				//i2f
+				//i2l
+				//i2s
+				//iadd
+				//iaload
+				//iand
+				//iastore
+
+				case 0x2: 
+					command.name = 'iconst_m1';
+					break;
+				case 0x3: 
+					command.name = 'iconst_0';
+					break;
+				case 0x4: 
+					command.name = 'iconst_1';
+					break;
+				case 0x5: 
+					command.name = 'iconst_2';
+					break;
+				case 0x6:
+					command.name = 'iconst_3';
+					break;
+				case 0x7: 
+					command.name = 'iconst_4';
+					break;
+				case 0x8: 
+					command.name = 'iconst_5';
+					break;
+
+				//idiv
+				//if_acmp_cond
+				//if_icmp_cond
+
+				case 0x99: 
+					command.name = 'ifeq';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0x9a: 
+					command.name = 'ifne';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0x9b: 
+					command.name = 'iflt';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0x9c: 
+					command.name = 'ifge';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0x9d: 
+					command.name = 'ifgt';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0x9e: 
+					command.name = 'ifle';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+
+				//ifnonnull
+				//ifnull
+				//iinc
+				//iload
+				//iload_n
+				//imul
+				//ineg
+				//instanceof
+				//invokedynamic
+				//invokeinterface
+				case 0xb7: 
+					command.name = 'invokespecial';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0xb8: 
+					command.name = 'invokestatic';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				case 0xb6: 
+					command.name = 'invokevirtual';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				//ior
+				//irem
+				//ireturn
+				//ishl
+				//ishr
+				//istore
+				//istore_n
+				//isub
+				//iushr
+				//ixor
+				//jsr
+				//jsr_w
+				//l2d
+				//l2f 
+				//l2i
+				//ladd
+				//laload
+				//land
+				//lastore
+				//lcmp
+				//lconst_l
+				case 0x12: 
+					command.name = 'ldc';
+					command.param.push(codes[i++]);
+					break;
+				//ldc_w
+				//ldc2_w
+				//ldiv
+				//lload
+				//lload_n
+				//lmul
+				//lneg
+				//lookupswitch
+				//lor
+				//lrem
+				//lreturn
+				//lshl
+				//lshr
+				//lstore
+				//lstore_n
+				//lsub
+				//lushr
+				//lxor
+				//monitorenter
+				//monitorexit
+				//multianewarray
+				//new
+				//newarray
+				case 0x0: 
+					command.name = 'nop';
+					break;
+				case 0x57: 
+					command.name = 'pop';
+					break;
+				case 0x58: 
+					command.name = 'pop2';
+					break;
+				//putfield
+				case 0xb3: 
+					command.name = 'putstatic';
+					command.param.push(codes[i++]);
+					command.param.push(codes[i++]);
+					break;
+				//ret
+				case 0xb1: 
+					command.name = 'return';
+					break;
+				//saload
+				//sastore
+				//sipush
+				//swap
+				//tableswitch
+				//wide
+
+				default: 
+					console.log('unknown code command: '+command.name);
+					i = codes.length;
+					break;
+			}
+
+			operations.push(command);
+		}
+
+		return operations;
+	};
+
+	Kvm.Class = function() 
 	{ 
 		return {
-			blob: blob,
+
 			buffer: false,
-
 			position: 0,
-
+			
 			header: {},
+
+			name: null,
+			constants: {},
+			methods: {},
+			fields: {},
 
 			readByte: function() 
 			{
@@ -222,6 +504,37 @@ if (window.jQuery === undefined) jQuery = $ = {};
 				return constant;
 			},
 
+			getReadableAccessType: function(access)
+			{
+				var output = [];
+				if(access & 0x1) output.push('public');
+				if(access & 0x2) output.push('private');
+				if(access & 0x4) output.push('protected');
+				if(access & 0x8) output.push('static');
+				if(access & 0x10) output.push('final');
+				if(access & 0x20) output.push('super');
+				if(access & 0x40) output.push('volatile');
+				if(access & 0x80) output.push('transient');
+				if(access & 0x200) output.push('interface');
+				if(access & 0x400) output.push('abstract');
+				if(access & 0x1000) output.push('synthetic');
+				if(access & 0x2000) output.push('annotation');
+				if(access & 0x4000) output.push('enum');
+
+				return output.join(' ');
+			},
+
+			getReadableConstantClass: function(number)
+			{
+				var className = this.header.constantPool[number].nameIndex;
+				return this.header.constantPool[className].value;
+			},
+
+			getReadableStringType: function(number)
+			{
+				return this.header.constantPool[number].value;
+			},
+
 			initSuccess: function()
 			{
 				this.header.magic = ['0x'+this.readByte().toString(16), '0x'+this.readByte().toString(16), '0x'+this.readByte().toString(16), '0x'+this.readByte().toString(16)];
@@ -290,11 +603,46 @@ if (window.jQuery === undefined) jQuery = $ = {};
 					this.header.attributes.attributes.push(this.readAttribute());
 				}
 
-				console.log(this.header);
-				console.log(this.buffer);
+				this.decompiled = {};
+				this.decompiled.accessFlags = this.getReadableAccessType(this.header.accessFlags);
+				this.decompiled.class = this.getReadableConstantClass(this.header.thisClass);
+				this.decompiled.super = this.getReadableConstantClass(this.header.superClass);
+				this.decompiled.fields = [];
+				this.decompiled.methods = [];
+
+				for(var i=0;i<this.header.fieldsCount;i++)
+				{
+					var field = {};
+
+					field.accessFlags = this.getReadableAccessType(this.header.fields[i].accessFlags);
+					field.name = this.getReadableStringType(this.header.fields[i].nameIndex);
+					field.description = this.getReadableStringType(this.header.fields[i].descriptorIndex);
+
+					this.decompiled.fields.push(field);
+				}
+
+				for(var i=0;i<this.header.methodsCount;i++)
+				{
+					var method = {};
+
+					method.accessFlags = this.getReadableAccessType(this.header.methods[i].accessFlags);
+					method.name = this.getReadableStringType(this.header.methods[i].nameIndex);
+					method.description = this.getReadableStringType(this.header.methods[i].descriptorIndex);
+
+					// attribute name must be 'Code' name
+					method.code = Kvm.Instructions(this.header.methods[i].attributes[0].code.codes);
+					this.decompiled.methods.push(method);
+				}
+
+				this.name = this.decompiled.class;
+				this.constants = this.header.constantPool;
+				this.fields = this.decompiled.fields;
+				this.methods = this.decompiled.methods;
+
+				Kvm.Core.appendClass(this);
 			},
 
-			init: function()
+			init: function(blob)
 			{
 				var reader = new FileReader();
 				var current = this;
@@ -302,10 +650,222 @@ if (window.jQuery === undefined) jQuery = $ = {};
 					current.buffer = new Uint8Array(reader.result);
 					current.initSuccess();
 				};
-				reader.readAsArrayBuffer(this.blob);
+				reader.readAsArrayBuffer(blob);
 			}
 		};
 	};
+
+	Kvm.Machine = {
+		run: function(runClass)
+		{
+			var constructor = null;
+			for(var i=0;i<runClass.methods.length;i++)
+				if(runClass.methods[i].name == '<init>')
+				{
+					constructor = runClass.methods[i];
+				}
+
+			this.runMethod(constructor, runClass);
+		},
+
+		runMethod: function(method, callerClass)
+		{
+			//console.log(method);
+			this.runInstructions(method.code, callerClass);
+		},
+
+		runInstructions: function(instructions, callerClass)
+		{
+			for(var i=0;i<instructions.length;i++)
+			{
+				var command = instructions[i];
+
+				switch(command.name)
+				{
+					//aaload
+					//aastore
+					//aconst_null
+					//aload
+					//aload_0
+					//aload_1
+					//aload_2
+					//aload_3
+					//anewarray
+					//areturn
+					//arraylength
+					//astore
+					//astore_n
+					//athrow
+					//baload
+					//bastore
+					//bipush
+					//caload
+					//castore
+					//checkcast
+					//d2f
+					//d2i
+					//d2l
+					//dadd
+					//daload
+					//dastore
+					//dcmp_op
+					//dconst_d
+					//ddiv
+					//dload
+					//dload_n
+					//dmul
+					//dneg
+					//drem
+					//dreturn
+					//dstore
+					//dstore_n
+					//dsub
+					//dup
+					//dup_x1
+					//dup_x2
+					//dup2
+					//dup2_x1
+					//dup2_x2
+					//f2d
+					//f2i
+					//f2l
+					//fadd
+					//faload
+					//fastore
+					//fcmp_op
+					//fconst_f
+					//fdiv
+					//fload
+					//fload_n
+					//fmul
+					//fneg
+					//frem
+					//freturn
+					//fstore
+					//fstore_n
+					//fsub
+					//getfield
+					//getstatic
+					//goto
+					//goto_w
+					//i2b
+					//i2c
+					//i2d
+					//i2f
+					//i2l
+					//i2s
+					//iadd
+					//iaload
+					//iand
+					//iastore
+					//iconst_m1
+					//iconst_0
+					//iconst_1
+					//iconst_2
+					//iconst_3
+					//iconst_4
+					//iconst_5
+					//idiv
+					//if_acmp_cond
+					//if_icmp_cond
+					//ifeq
+					//ifne
+					//iflt
+					//ifge
+					//ifgt
+					//ifle
+					//ifnonnull
+					//ifnull
+					//iinc
+					//iload
+					//iload_n
+					//imul
+					//ineg
+					//instanceof
+					//invokedynamic
+					//invokeinterface
+					case 'invokespecial':
+						var poolIndex = command.param[0] * 256 + command.param[1];
+						var constantMethod = callerClass.constants[poolIndex];
+						var constantClass = callerClass.constants[constantMethod.classIndex];
+						var constantNameType = callerClass.constants[constantMethod.nameTypeIndex];
+
+						var constantClassName = callerClass.constants[constantClass.nameIndex].value;
+						var constantMethodName = callerClass.constants[constantNameType.nameIndex].value;
+						var constantDescriptionName = callerClass.constants[constantNameType.descriptionIndex].value;
+
+						console.log(constantClassName);
+						console.log(constantMethodName);
+						console.log(constantDescriptionName);
+						break;
+					//invokestatic
+					//invokevirtual
+					//ior
+					//irem
+					//ireturn
+					//ishl
+					//ishr
+					//istore
+					//istore_n
+					//isub
+					//iushr
+					//ixor
+					//jsr
+					//jsr_w
+					//l2d
+					//l2f 
+					//l2i
+					//ladd
+					//laload
+					//land
+					//lastore
+					//lcmp
+					//lconst_l
+					//ldc
+					//ldc_w
+					//ldc2_w
+					//ldiv
+					//lload
+					//lload_n
+					//lmul
+					//lneg
+					//lookupswitch
+					//lor
+					//lrem
+					//lreturn
+					//lshl
+					//lshr
+					//lstore
+					//lstore_n
+					//lsub
+					//lushr
+					//lxor
+					//monitorenter
+					//monitorexit
+					//multianewarray
+					//new
+					//newarray
+					//nop
+					//pop
+					//pop2
+					//putfield
+					//putstatic
+					//ret
+					//return
+					//saload
+					//sastore
+					//sipush
+					//swap
+					//tableswitch
+					//wide
+
+					default:
+						console.log('no command processing: '+command.name);
+						break;
+				}
+			}
+		}
+	},
 
 	Kvm.Core = {
 		manifest: {},
@@ -313,6 +873,18 @@ if (window.jQuery === undefined) jQuery = $ = {};
 		name: false,
 		icon: false,
 		baseClass: false,
+
+		classes: {},
+
+		appendClass: function(insertClass)
+		{
+			this.classes[insertClass.name] = insertClass;
+			console.log('class append');
+			console.log(insertClass);
+
+			if(insertClass.name == this.baseClass)
+				Kvm.Machine.run(this.classes[this.baseClass]);
+		},
 
 		run: function(base64Blob)
 		{
@@ -334,8 +906,8 @@ if (window.jQuery === undefined) jQuery = $ = {};
 			this.icon = midletInfo[1].replace(/^\//g, '');	// remove leading slash in path
 			this.baseClass = midletInfo[2];
 
-			var baseClass = new Kvm.Class(Kvm.Loader.files[this.baseClass+'.class'].data);
-			baseClass.init();
+			var baseClass = new Kvm.Class();
+			baseClass.init(Kvm.Loader.files[this.baseClass+'.class'].data);
 		},
 
 		loadFile(filename)
